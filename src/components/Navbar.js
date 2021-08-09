45953ff6eed4b640b4418b262fa41cf5cfd278f2
riverdigger <niallminer@gmail.com>
2021-08-09 16:18:13 -0500

    Fixed navbar for mobile

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,17 +4,9 @@ import { PaperClipIcon } from "@heroicons/react/solid";
 import React from "react";
 
 export default function Navbar() {
-  function addBounce(e) {
-    e.target.classList.add('animate-bounce');
-    // e.target.childNodes[0].classList.add('animate-bounce');
-  }
-  function removeBounce(e) {
-    e.target.classList.remove('animate-bounce');
-    // e.target.childNodes[0].classList.remove('animate-bounce');
-  }
   return (
-    <header className="bg-white md:sticky top-0 z-10">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <header className="bg-white sm:sticky top-0 z-10">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col sm:flex-row items-center">
         <a href="#about">
           <img
           className="object-cover object-center rounded"
@@ -22,7 +14,7 @@ export default function Navbar() {
           src="./logo_small.png"
           />
         </a>
-        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700	flex flex-wrap items-center text-base justify-center">
+        <nav className="sm:mr-auto sm:ml-4 sm:py-1 sm:pl-4 sm:border-l sm:border-gray-700	flex flex-wrap items-center text-base justify-center">
           <a href="#projects" className="mr-5 hover:text-gray-400">
             Work
           </a>
@@ -32,11 +24,21 @@ export default function Navbar() {
           <a href="#testimonials" className="mr-5 hover:text-gray-400">
             Projects
           </a>
+          <a href="#contact" className="mr-5 hover:text-gray-400">
+            Contact
+          </a>
+          <a
+            href="./resume_niall_miner.pdf"
+            target="_blank"
+            className="sm:hidden inline-flex items-center text-green-400">
+            Resume
+            <PaperClipIcon className="w-4 h-4 ml-1" />
+          </a>
         </nav>
         <a
           href="./resume_niall_miner.pdf"
           target="_blank"
-          className="inline-flex items-center text-white bg-green-400 transform -skew-x-12 px-3 hover:bg-green-500 text-lg">
+          className="sm:inline-flex hidden items-center text-white bg-green-400 transform sm:-skew-x-12 sm:rounded-none rounded-md px-3 hover:bg-green-500 text-lg sm:mt-0 mt-4">
           Resume
           <PaperClipIcon className="w-4 h-4 ml-1" />
         </a>
